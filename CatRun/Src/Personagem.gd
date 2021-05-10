83f9c3a9921f590d34023ec2c9507c3601605f35
kelvys <kelvysboniek.pantaleao@philips.com>
2021-05-10 18:06:26 -0300

    fix movement

--- CatRun/Src/Personagem.gd
+++ CatRun/Src/Personagem.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 
-export var gravidade = 800.0 
-export var velocidade = Vector2(300.0, 800.0)
-export var desaceleracao = 10.0
-export var forca_pulo = 350.0
-var movimento = Vector2.ZERO
+export var gravidade = 4000
+export var velocidade = 800
+export var forca_pulo = 900
+export var friccao = 0.25
+export var aceleracao = 0.1
 
+var movimento = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,35 +16,27 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	var direcao = calcular_direcao()
+	calcular_movimento(delta)
 	
-	movimento = calcular_movimento(direcao, movimento, delta)
-		
-	move_and_slide(movimento, Vector2.UP)
+	movimento = move_and_slide(movimento, Vector2.UP)
 	
-func calcular_direcao():
-	return Vector2(
-		Input.get_action_strength("Mover_Direita") - Input.get_action_strength("Mover_Esquerda"),
-		-1.0 if Input.is_action_just_pressed("Pular") and is_on_floor() else 1.0
-	)
+	if Input.is_action_just_pressed("Pular"):
+		if is_on_floor():
+			movimento.y = forca_pulo * -1
 	
-func calcular_movimento(direcao, movimento, delta):
-		
 	
-	var novo_movimento = movimento
+func calcular_movimento(delta):
 	
-	print(abs(novo_movimento.x))
-		
-	if direcao.y == 1.0:
-		novo_movimento.y = novo_movimento.y + (gravidade * delta) if novo_movimento.y < velocidade.y else velocidade.y
+	var direcao = 0
+	
+	if Input.is_action_pressed("Mover_Direita"):
+		direcao += 1
+	if Input.is_action_pressed("Mover_Esquerda"):
+		direcao -= 1
+	
+	if direcao != 0:
+		movimento.x = lerp(movimento.x, direcao * velocidade, aceleracao)
 	else:
-		novo_movimento.y = forca_pulo * direcao.y
+		movimento.x = lerp(movimento.x, 0, friccao)
 		
-	if direcao.x != 0.0:
-		novo_movimento.x = novo_movimento.x + (direcao.x * velocidade.x * delta) if abs(novo_movimento.x) < velocidade.x else direcao.x * velocidade.x
-	else: 
-		novo_movimento.x -= desaceleracao * novo_movimento.x * delta
-		
-	return novo_movimento
-	
-	
+	movimento.y += gravidade * delta
